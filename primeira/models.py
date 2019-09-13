c6a02d88b636b842e39a98e90a5b09566120b568
analais17 <analaismoraes17@gmail.com>
2019-09-13 18:52:59 -0300

    segundo commit

--- primeira/models.py
+++ primeira/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
+
+class Carrinho_De_Compras (models.Model):
+    capacidade=models.CharField(max_length=50)
+    valor_carrinho = models.CharField(max_length=11)
+  
+
+class Produto (models.Model):
+    nome = models.CharField(max_length=50)
+    preco= models.CharField(max_length=11)
+    quantidade= models.CharField(max_length=10)
+    carrinho_de_compras=models.ForeignKey(Carrinho_De_Compras, on_delete=models.CASCADE) 
+   
+
+class Cliente (models.Model):
+    nome = models.CharField(max_length=50)
+    endereco = models.CharField(max_length=11)
+    cpf= models.CharField(max_length=10)
+    carrinho_de_compras=models.OneToOneField(Carrinho_De_Compras,on_delete=models.CASCADE, primary_key=True)
+
+
+
+
+
 # Create your models here.
